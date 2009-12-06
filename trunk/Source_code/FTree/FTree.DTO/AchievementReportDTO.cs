172ea6336ffb362958183b74338f8a676d24aff9
dangphat29988 <dangphat29988@c5328d58-d788-11de-89f4-0baa3b62fdab>
2009-12-06 07:28:37 +0000

    AchievementReport: 50% - chua xu ly duoc truy van trong linq FromYear < Year(date) < ToYear - chua xu ly duoc cau truc tra ve cua linq (xem ham AchievementReport()) - db co bo qhe khoa ngoai cua Member_event voi Member -> de them du lieu test, co the edit sau

--- trunk/Source_code/FTree/FTree.DTO/AchievementReportDTO.cs
+++ trunk/Source_code/FTree/FTree.DTO/AchievementReportDTO.cs
@@ -7,8 +7,11 @@ namespace FTree.DTO
 {
     public class AchievementReportDTO : DataTransferObject
     {
-        public DateTime AchievementDate { get; set; }
-        public AchievementType AchievementType { get; set; }
-        public int Quantity { get; set; }
+        //public DateTime AchievementDate { get; set; }
+        //public AchievementType AchievementType { get; set; }
+        //public int Quantity { get; set; }
+        //public int AchievementYear { get; set; }
+        public int Achievement;
+        public int Total;
     }
 }
